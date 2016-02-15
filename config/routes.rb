991d2a36f8fa98938baf103136864f949ee11aae
Rafael Mateus <rafaelmateus@hotmail.com.br>
2016-02-15 19:22:12 -0200

    Created UsersController

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
 
   namespace :v1, defaults: { format: :json } do
     resource :login, only: [:create], controller: :sessions
+    resources :users, only: [:create]
+    resources :stories, only: [:index, :show, :create]
   end
 end
